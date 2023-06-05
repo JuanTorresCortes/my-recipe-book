397a08934cfab6a1de5ff3c310f0a60edcfaa7c2
Juan <juan.giana8589@gmail.com>
2023-06-05 09:11:33 -0500

    fixed hanging on updateRecipee and getRecipeByID due to syntax error in if(statement)

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -1,66 +1,78 @@
 const { json } = require("express");
-const Recipe = require("../model/Recipe")
+const Recipe = require("../model/Recipe");
 
 const createRecipe = async (req, res) => {
-    try {
-        const newRecipe = await new Recipe(req.body);
-        const saveRecipe = await newRecipe.save();       
-    res.status(200).json({ success: true, data: saveRecipe});
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message })
-    }
-}
+  // http://localhost:3000/recipes/new-recipe
+  try {
+    const newRecipe = await new Recipe(req.body);
+    const saveRecipe = await newRecipe.save();
+    res.status(200).json({ success: true, data: saveRecipe });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const getAll = async (req, res) => {
-    try {
-        const allRecipes = await Recipe.find({})
-        res.status(200).json({ success: true, data: allRecipes})
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message })
-    }
-}
+  // http://localhost:3000/recipes/getAllRecipes
+  try {
+    const allRecipes = await Recipe.find({});
+    res.status(200).json({ success: true, data: allRecipes });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const getRecipeByID = async (req, res) => {
-   // http://localhost:3000/recipes/getRecipeID/> recipe id here < 
-   try {
-    const targetID = req.params.id;
-    const targetRecipe = await Recipe.findById({ _id: targetID})
-
-    if(!targetRecipe) {
-       return res.status(400),json({ success: false, message: "recipe not found"})
+  // http://localhost:3000/recipes/getRecipeID/> recipe id here <
+  try {
+    const targetRecipe = await Recipe.findById({ _id: req.params.id });
+    if (!targetRecipe) {
+      return res
+        .status(400)
+        .json({ success: false, message: "recipe not found" });
     }
-    res.status(200).json(({ success: true, data: targetRecipe}))
-   } catch (error) {
-    res.status(500).json({ success: false, message: error.message })
-   }
-}
+    res.status(200).json({ success: true, data: targetRecipe });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const updateRecipe = async (req, res) => {
-    // http://localhost:3000/recipes/updateRecipe/ > recipe id here <
-    try {
-        const targetRecipe = await Recipe.findByIdAndUpdate({ _id: req.params.id }, req.body);
-
-        if(!targetRecipe) {
-            return res.status(400),json({ success: false, message: "recipe not found"})
-        }
-        res.status(200).json({ success: true, data: targetRecipe})
-    } catch (error) {
-        res.status(500).json({ success: true, message: error.message})
+  // http://localhost:3000/recipes/updateRecipe/ > recipe id here <
+  try {
+    const targetRecipe = await Recipe.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body
+    );
+    if (!targetRecipe) {
+      return res
+        .status(400)
+        .json({ success: false, message: "recipe not found" });
     }
-}
+    res.status(200).json({ success: true, data: targetRecipe });
+  } catch (error) {
+    res.status(500).json({ success: true, message: error.message });
+  }
+};
 
 const deleteRecipe = async (req, res) => {
-    try {
-        const targetID = req.params.id;
-        const targetRecipe = await Recipe.findByIdAndDelete({ _id: targetID });
-        
-        if(!targetRecipe){
-            return res.status(400).json({ success: false, message: " recipe not found" });
-        }
-        
-        res.status(200).json({ success: true, data: targetRecipe })
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message })
+  // http://localhost:3000/recipes/deleteRecipe/ > recipe id here <
+  try {
+    const targetRecipe = await Recipe.findByIdAndDelete({ _id: req.params.id });
+    if (!targetRecipe) {
+      return res
+        .status(400)
+        .json({ success: false, message: " recipe not found" });
     }
-}
-module.exports = {createRecipe, getAll, getRecipeByID, updateRecipe, deleteRecipe}; 
+    res.status(200).json({ success: true, data: targetRecipe });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+module.exports = {
+  createRecipe,
+  getAll,
+  getRecipeByID,
+  updateRecipe,
+  deleteRecipe,
+};
