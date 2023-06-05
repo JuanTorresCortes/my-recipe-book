0021d88dd4acff1f8e61f1533599954f8f146cc3
Juan <juan.giana8589@gmail.com>
2023-06-04 20:45:57 -0500

    added updateRecipe and deleteRecipe

--- controller/recipeController.js
+++ controller/recipeController.js
@@ -1,3 +1,4 @@
+const { json } = require("express");
 const Recipe = require("../model/Recipe")
 
 const createRecipe = async (req, res) => {
@@ -10,4 +11,56 @@ const createRecipe = async (req, res) => {
     }
 }
 
-module.exports = {createRecipe};
+const getAll = async (req, res) => {
+    try {
+        const allRecipes = await Recipe.find({})
+        res.status(200).json({ success: true, data: allRecipes})
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
+
+const getRecipeByID = async (req, res) => {
+   // http://localhost:3000/recipes/getRecipeID/> recipe id here < 
+   try {
+    const targetID = req.params.id;
+    const targetRecipe = await Recipe.findById({ _id: targetID})
+
+    if(!targetRecipe) {
+       return res.status(400),json({ success: false, message: "recipe not found"})
+    }
+    res.status(200).json(({ success: true, data: targetRecipe}))
+   } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+   }
+}
+
+const updateRecipe = async (req, res) => {
+    // http://localhost:3000/recipes/updateRecipe/ > recipe id here <
+    try {
+        const targetRecipe = await Recipe.findByIdAndUpdate({ _id: req.params.id }, req.body);
+
+        if(!targetRecipe) {
+            return res.status(400),json({ success: false, message: "recipe not found"})
+        }
+        res.status(200).json({ success: true, data: targetRecipe})
+    } catch (error) {
+        res.status(500).json({ success: true, message: error.message})
+    }
+}
+
+const deleteRecipe = async (req, res) => {
+    try {
+        const targetID = req.params.id;
+        const targetRecipe = await Recipe.findByIdAndDelete({ _id: targetID });
+        
+        if(!targetRecipe){
+            return res.status(400).json({ success: false, message: " recipe not found" });
+        }
+        
+        res.status(200).json({ success: true, data: targetRecipe })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
+}
+module.exports = {createRecipe, getAll, getRecipeByID, updateRecipe, deleteRecipe}; 
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const  router = express.Router();
+const router = express.Router();
 
-const {createRecipe} = require("../controller/recipeController");
+const { createRecipe, getAll, getRecipeByID, updateRecipe, deleteRecipe} = require("../controller/recipeController");
 
-router.post("/new-recipe", createRecipe);
+router.post( "/new-recipe", createRecipe );
+router.get ( "/getAllRecipes", getAll );
+router.get( "/getRecipeID/:id", getRecipeByID );
+router.put( "/updateRecipe/:id", updateRecipe );
+router.delete( "/deleteRecipe/:id", deleteRecipe )
 
 module.exports = router;
